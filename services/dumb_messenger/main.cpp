f9ae2e309681fe973a7d1111185aafb65fdae498
darksider <darksider@darksiderPC>
2016-02-25 19:37:10 +0300

    new project

---
+++ services/dumb_messenger/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: darksider
+ *
+ * Created on February 25, 2016, 7:27 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+
+    return 0;
+}
+
