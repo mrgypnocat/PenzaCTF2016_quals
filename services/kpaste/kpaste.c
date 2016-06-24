229cbcbde72ca0e2dff758ac01889b9b961dd9a1
Mikhail Klementyev <jollheef@riseup.net>
2016-06-25 01:59:51 +0300

    Base concept

---
+++ services/kpaste/kpaste.c
@@ -0,0 +1,186 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/seq_file.h>
+#include <linux/slab.h>
+#include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <linux/list.h>
+#include <linux/kthread.h>
+
+#include <net/sock.h>
+#include <net/tcp.h>
+
+
+#define TRACE(fmt, arg...)					\
+	printk("%s:%d:%s:%s " fmt "\n",				\
+	       (strrchr(__FILE__, '/') ?			\
+		strrchr(__FILE__, '/') + 1 : __FILE__),		\
+	       __LINE__, __FUNCTION__, current->comm, ##arg)
+
+#define KPASTE_PORT 393
+
+struct kpaste_service {
+        struct socket *sock;
+};
+
+static struct kpaste_service kpaste_service;
+
+struct workqueue_struct *wq;
+
+static int kpaste_send(struct socket *sock, char *buf, int len)
+{
+        struct kvec kv = { .iov_base = buf, .iov_len = len };
+        struct msghdr msg = { 0 };
+
+        iov_iter_kvec(&msg.msg_iter, READ | ITER_KVEC, &kv, 1, len);
+
+        return kernel_sendmsg(sock, &msg, &kv, 1, len);
+}
+
+static int kpaste_recv(struct socket *sock, char *buf, int len)
+{
+        struct kvec kv = { .iov_base = buf, .iov_len = len };
+        struct msghdr msg = { 0 };
+
+        iov_iter_kvec(&msg.msg_iter, READ | ITER_KVEC, &kv, 1, len);
+
+        return kernel_recvmsg(sock, &msg, &kv, 1, len, msg.msg_flags);
+}
+
+struct kpaste_work
+{
+        struct work_struct work;
+        struct socket *sock;
+};
+
+static void kpaste_interaction(struct work_struct *work)
+{
+        struct kpaste_work *kw = (typeof(kw))work;
+
+        TRACE("kw: %p kw->sock: %p", kw, kw->sock);
+
+        kpaste_send(kw->sock, "Hello!\n", sizeof("Hello!\n"));
+
+        const int buf_len = 1024;
+        char* buf = kmalloc(buf_len, GFP_KERNEL);
+        TRACE("kpaste_recv: %ld", kpaste_recv(kw->sock, buf, buf_len));
+        TRACE("recv: %s", buf);
+
+        kfree(buf);
+
+        kw->sock->ops->shutdown(kw->sock, SHUT_RDWR);
+
+        TRACE("");
+
+        kfree(work);
+}
+
+static int kpaste_accept(void)
+{
+        struct socket *client_sock;
+
+        int ret = sock_create(PF_INET, SOCK_STREAM, IPPROTO_TCP, &client_sock);
+        if (IS_ERR_VALUE(ret)) {
+                TRACE("sock_create: %d", ret);
+                goto exit;
+        }
+
+        ret = kpaste_service.sock->ops->accept(kpaste_service.sock,
+                                               client_sock, O_NONBLOCK);
+        if (IS_ERR_VALUE(ret))
+                goto exit;
+
+        TRACE("success accept");
+
+        struct kpaste_work *kw = kmalloc(sizeof(typeof(*kw)), GFP_KERNEL);
+        if (!kw) {
+                TRACE("enomem");
+                ret = -ENOMEM;
+                goto exit;
+        }
+
+        INIT_WORK((struct work_struct*)kw, kpaste_interaction);
+
+        kw->sock = client_sock;
+
+        queue_work(wq, (struct work_struct *)kw);
+exit:
+        return ret;
+}
+
+/* threaded */
+static int kpaste_listener(void *unused)
+{
+        TRACE("");
+        int ret;
+
+        while (1) {
+                ret = kpaste_accept();
+                if (IS_ERR_VALUE(ret) && ret != -EAGAIN) {
+                        TRACE("kpaste_accept: %d", ret);                        
+                        goto exit;
+                }
+
+                set_current_state(TASK_INTERRUPTIBLE);                
+                schedule_timeout(HZ/1000);
+                set_current_state(TASK_RUNNING);
+        }
+
+exit:
+        return ret;
+}
+
+static int __init kpaste_init(void)
+{
+	TRACE("");
+        int ret;
+
+        wq = create_workqueue("kpaste_queue");
+        if (IS_ERR(wq)) {
+                ret = -EFAULT;
+                TRACE("create_workqueue: %ld", PTR_ERR(wq));
+                goto exit;
+        }
+
+        struct socket *sock;
+
+        ret = sock_create(PF_INET, SOCK_STREAM, IPPROTO_TCP, &sock);
+
+        if (IS_ERR_VALUE(ret)) {
+                TRACE("sock_create: %d", ret);
+                goto exit;
+        }
+
+        kpaste_service.sock = sock;
+
+        struct sockaddr_in addr = { 0 };
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        addr.sin_port = htons(KPASTE_PORT);
+
+        ret = sock->ops->bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+        if (IS_ERR_VALUE(ret)) {
+                TRACE("bind: %d", ret);
+                goto exit;
+        }
+
+        ret = sock->ops->listen(sock, SOMAXCONN);
+        if (IS_ERR_VALUE(ret)) {
+                TRACE("listen: %d", ret);
+                goto exit;
+        }
+
+        kthread_run(kpaste_listener, NULL, "kpaste_listener");
+
+exit:
+	return ret;
+}
+
+static void __exit kpaste_exit(void)
+{
+	TRACE("");
+}
+
+module_init(kpaste_init)
+module_exit(kpaste_exit)
+MODULE_LICENSE("GPL");
